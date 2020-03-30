8afbe8e7103921ab05d45d9d792aefb9ca87e83a
Lucas Krueger <lucas.krueger@brconselhos.com>
2020-03-30 15:48:39 -0300

    events

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,19 +6,8 @@ class CalcControler{
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
-    }
-
-    initialize(){
-        this.setDisplayDateTime();
-        setInterval(() => {
-            this.setDisplayDateTime();
-        },1000);
-    }
-
-    setDisplayDateTime() {
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
-        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-    }
+        this.initButtonsEvents();
+    }     
 
     get displayTime(){
         return this._timeEl.innerHTML;
@@ -50,5 +39,37 @@ class CalcControler{
     
     set currentDate(value){
         this._currentDate = value;
+    }
+
+    initialize(){
+        this.setDisplayDateTime();
+        setInterval(() => {
+            this.setDisplayDateTime();
+        },1000);
+    }
+    
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+        buttons.forEach((btn, index) =>{        
+            this.addEventListenerAll(btn, 'click drag', e=>{
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+        
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            });
+        })
+    }   
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
     }
 }
